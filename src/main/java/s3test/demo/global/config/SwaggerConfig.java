47ed147c402779738086246910d49ce6ca7fc1a0
ykh4663 <superho7@gmail.com>
2025-07-10 11:03:58 +0900

    test14-> 정적파일 하나 추가

--- src/main/java/s3test/demo/global/config/SwaggerConfig.java
+++ src/main/java/s3test/demo/global/config/SwaggerConfig.java
@@ -3,16 +3,9 @@ package s3test.demo.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
+
+
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-import io.swagger.v3.oas.models.OpenAPI;
 
 
 @OpenAPIDefinition(
