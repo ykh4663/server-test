d8eed65621d31f9669941a1f3648c851c42cb8b3
ykh4663 <superho7@gmail.com>
2025-07-10 11:13:02 +0900

    test16-> swagger https로 매핑

--- src/main/java/s3test/demo/global/config/SwaggerConfig.java
+++ src/main/java/s3test/demo/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -13,7 +14,10 @@ import org.springframework.context.annotation.Configuration;
                 title = "Spring Study",
                 version = "1.0v",
                 description = "회원 관리 API"
-        )
+        ),
+        servers = {
+                @Server(url = "https://api.post-server.p-e.kr", description = "Default Server URL")
+        }
 )
 @Configuration
 public class SwaggerConfig {
